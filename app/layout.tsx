aeba8495df8425136546556b789ec6c4244c242c
Moktadir Rashid <73337818+moktadirr@users.noreply.github.com>
2025-07-24 01:23:51 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,13 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/favicon.ico" />
 
         {/* Google Analytics */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" strategy="afterInteractive" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-987XN0ZZEG" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'GA_MEASUREMENT_ID');
+            gtag('config', 'G-987XN0ZZEG');
           `}
         </Script>
       </head>
